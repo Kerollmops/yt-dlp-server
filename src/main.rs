3cb334a1fc81b66dc431219d9c70375b822a0ab8
Clément Renault <clement@meilisearch.com>
2023-01-23 18:27:22 +0100

    Do the download in background

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::path::PathBuf;
 
 use anyhow::bail;
 use axum::extract::{Query, State};
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use clap::Parser;
 use serde::Deserialize;
 use tokio::process::Command;
+use tokio::task;
+use tracing::{debug, error, info, trace};
 
 /// The server that listens and downloads videos by using yt-dlp.
 #[derive(Parser, Debug)]
@@ -37,22 +37,28 @@ struct AppState {
 async fn download_url(
     State(AppState { download_folder }): State<AppState>,
     Query(DownloadURL { url }): Query<DownloadURL>,
-) -> impl IntoResponse {
-    let result = Command::new("yt-dlp")
-        .args(["--paths", &download_folder.display().to_string()])
-        .arg(&url)
-        .output()
-        .await;
+) {
+    trace!("Started downloading {url:?}...");
 
-    match result {
-        Ok(out) if out.status.success() => (StatusCode::OK, String::new()),
-        Ok(out) => (StatusCode::INTERNAL_SERVER_ERROR, String::from_utf8(out.stderr).unwrap()),
-        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-    }
+    task::spawn(async move {
+        let result = Command::new("yt-dlp")
+            .args(["--paths", &download_folder.display().to_string()])
+            .arg(&url)
+            .status()
+            .await;
+
+        match result {
+            Ok(s) if !s.success() => error!("There is an issue downloading {url:?}, status: {s}"),
+            Err(err) => error!("There is an issue downloading {url:?}: {err:?}"),
+            _ => info!("Finished downloading {url:?}"),
+        }
+    });
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let Args { listen, download_folder } = Args::parse();
     let app_state = AppState { download_folder };
 
@@ -60,13 +66,13 @@ async fn main() -> anyhow::Result<()> {
         Ok(output) => {
             let version = String::from_utf8(output.stdout).unwrap();
             let version = version.lines().next().unwrap();
-            eprintln!("Running the server with `yt-dlp` version {version}");
+            debug!("Running the server with `yt-dlp` version {version}");
         }
         Err(e) => bail!("While running `yt-dlp --version`: {e}"),
     };
 
     let app = Router::new().route("/", get(download_url)).with_state(app_state);
-    eprintln!("Listening on {listen}...");
+    debug!("Listening on {listen}...");
     axum::Server::bind(&listen).serve(app.into_make_service()).await?;
 
     Ok(())
